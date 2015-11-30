1a390efa5c4c133493e1fdfb223bac715710d54d
Hitko Development <development@hitko.si>
2015-11-30 21:59:09 +0100

    Building, app signing, google play login

--- Android/ChemApp/src/main/java/com/hitkoDev/chemApp/MainActivity.java
+++ Android/ChemApp/src/main/java/com/hitkoDev/chemApp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hitkoDev.chemApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -23,20 +24,43 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
 /**
  *
  * @author hitno
  */
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements 
+        NavigationView.OnNavigationItemSelectedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
     
     private ArrayList<Level> levels = new ArrayList();
     private NavigationView navigationView;
     private boolean lvl;
     private int level;
     
+    private GoogleApiClient mGoogleApiClient;
+    private static int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInFlow = true;
+    private boolean mSignInClicked = false;
+    
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
+        
+        // Create the Google Api Client with access to the Play Games services
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                // add other APIs and scopes here as needed
+                .build();
+        
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -80,6 +104,84 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 System.out.println(response);
             }
         }).executeCached("level");
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+    
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // The player is signed in. Hide the sign-in button and allow the
+        // player to proceed.
+    }
+    
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure) {
+            // already resolving
+            return;
+        }
+
+        // if the sign-in button was clicked or if auto sign-in is enabled,
+        // launch the sign-in flow
+        if (mSignInClicked || mAutoStartSignInFlow) {
+            mAutoStartSignInFlow = false;
+            mSignInClicked = false;
+            mResolvingConnectionFailure = true;
+
+            // Attempt to resolve the connection failure using BaseGameUtils.
+            // The R.string.signin_other_error value should reference a generic
+            // error string in your strings.xml file, such as "There was
+            // an issue with sign-in, please try again later."
+            if (!BaseGameUtils.resolveConnectionFailure(this, mGoogleApiClient, connectionResult, RC_SIGN_IN, getString(R.string.signin_other_error))) {
+                mResolvingConnectionFailure = false;
+            }
+        }
+
+        // Put code here to display the sign-in button
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // Attempt to reconnect
+        mGoogleApiClient.connect();
+    }
+    
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            } else {
+                // Bring up an error dialog to alert the user that sign-in
+                // failed. The R.string.signin_failure should reference an error
+                // string in your strings.xml file that tells the user they
+                // could not be signed in, such as "Unable to sign in."
+                BaseGameUtils.showActivityResultError(this, requestCode, resultCode, R.string.signin_failure);
+            }
+        }
+    }
+    
+    // Call when the sign-in button is clicked
+    private void signInClicked() {
+        mSignInClicked = true;
+        mGoogleApiClient.connect();
+    }
+
+    // Call when the sign-out button is clicked
+    private void signOutclicked() {
+        mSignInClicked = false;
+        Games.signOut(mGoogleApiClient);
     }
 
     @Override
@@ -136,7 +238,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         
         if(lvl){
             
-            setLevel(id);
+            switch(id){
+                case R.id.nav_log_in_out:
+                    mSignInClicked = true;
+                    mGoogleApiClient.connect();
+                    break;
+                default:
+                    setLevel(id);
+                    break;
+            }
             
         } else {
 
@@ -169,10 +279,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 navigationView.getMenu().clear();
                 navigationView.inflateMenu(R.menu.drawer_select_level);
                 Menu m = navigationView.getMenu();
-                for(Level l : levels) m.add(R.id.nav_levels, l.getId(), 0, l.getName());
+                int i = 5;
+                for(Level l : levels) m.add(R.id.nav_levels, l.getId(), i++, l.getName());
             } else {
                 Menu m = navigationView.getMenu();
-                for(Level l : levels) if(m.findItem(l.getId()) == null) m.add(R.id.nav_levels, l.getId(), 0, l.getName());
+                int i = 5;
+                for(Level l : levels) if(m.findItem(l.getId()) == null) m.add(R.id.nav_levels, l.getId(), i++, l.getName());
             }
         } else {
             if(item == null){
